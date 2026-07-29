e96e52d649f7bd10d2284f998e3ac3e3d297cf02
Anonimus <baderialailatul491@gmail.com>
2026-07-29 14:06:16 +0700

    Update LibraryV2.lua

--- Library/LibraryV2.lua
+++ Library/LibraryV2.lua
@@ -1116,9 +1116,7 @@ end
 local FetchIcons, Icons = pcall(function()
 	return ((loadstring(
 		game:HttpGet(
-			"https://raw.githubusercontent.com/"
-			.. "deividcomsono/lucide-roblox-direct/"
-			.. "refs/heads/main/source.lua"
+			BaseURL
 		)
 	)))()
 end)
